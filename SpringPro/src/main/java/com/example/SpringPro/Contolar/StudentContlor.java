e1fb0752692b5e80832e7a510af7c846a4b721e3
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-06-06 22:47:18 +0400

    create restController

--- SpringPro/src/main/java/com/example/SpringPro/Contolar/StudentContlor.java
+++ SpringPro/src/main/java/com/example/SpringPro/Contolar/StudentContlor.java
@@ -1,8 +1,15 @@
 package com.example.SpringPro.Contolar;
 
+import com.example.SpringPro.Service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 
 public class StudentContlor {
+    @Autowired
+    private StudentService  studentService;
+
+
+
 }
