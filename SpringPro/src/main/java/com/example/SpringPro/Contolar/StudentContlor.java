720a537d0d9f92a5296d8ce66a4cdd50003fafe8
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-06-06 22:50:28 +0400

    postMapping

--- SpringPro/src/main/java/com/example/SpringPro/Contolar/StudentContlor.java
+++ SpringPro/src/main/java/com/example/SpringPro/Contolar/StudentContlor.java
@@ -1,7 +1,10 @@
 package com.example.SpringPro.Contolar;
 
+import com.example.SpringPro.Entity.Student;
 import com.example.SpringPro.Service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,6 +13,10 @@ public class StudentContlor {
     @Autowired
     private StudentService  studentService;
 
+    @PostMapping("/addStudent")
+    public Student postDetails(@RequestBody Student student) {
+
+    }
 
 
 }
