7dfb11843d585c7b08bcbb30d2aa6aa5b7c5e587
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-06-07 09:26:18 +0400

    create alist of student

--- SpringPro/src/main/java/com/example/SpringPro/Contolar/StudentContlor.java
+++ SpringPro/src/main/java/com/example/SpringPro/Contolar/StudentContlor.java
@@ -3,9 +3,7 @@ package com.example.SpringPro.Contolar;
 import com.example.SpringPro.Entity.Student;
 import com.example.SpringPro.Service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -24,6 +22,22 @@ public class StudentContlor {
     public List<Student> postDetails(@RequestBody List<Student> students) {
         return studentService.addListStudent(students);
 
+    }
+    @GetMapping("/students")
+    public List<Student> getStudents(@RequestBody List<Student> students) {
+        return students;
+    }
+    @GetMapping("/search")
+    public String search(@RequestParam String name) {
+        return name;
+    }
+    @DeleteMapping("/delete")
+    public String deleteStudent(@RequestParam int id) {
+        return "Student with id " + id + " deleted";
+    }
+    @GetMapping("/find/{id}")
+    public String findStudent(@PathVariable int id) {
+        return "Student found with id = " + id;
     }
 
 
